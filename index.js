4eba7606f64e9db75bb870c38994e279671c2806
johncutz <joaovitorcut@gmail.com>
2021-10-05 17:48:02 -0300

    Projeto JS

---
+++ index.js
@@ -0,0 +1,20 @@
+import { Cliente } from "./Cliente.js";
+import { Gerente } from "./Funcionarios/Gerente.js";
+import { Diretor } from "./Funcionarios/Diretor.js";
+import { SistemaAutenticacao } from "./SistemaAutenticacao.js";
+
+const diretor = new Diretor("Rodrigo", 10000, 12345678900);
+diretor.cadastrarSenha("12345");
+
+const gerente = new Gerente("Ricardo", 5000, 12378945601);
+gerente.cadastrarSenha("54321");
+
+const cliente = new Cliente("Lais", 78945612379, "456");
+
+const diretorEstaLogado = SistemaAutenticacao.login(diretor, "12345");
+const gerenteEstaLogado = SistemaAutenticacao.login(gerente, "54321");
+
+
+const clienteEstaLogado = SistemaAutenticacao.login(cliente, "456");
+
+console.log(diretorEstaLogado, gerenteEstaLogado, clienteEstaLogado);
